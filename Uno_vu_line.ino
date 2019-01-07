db425228af72f1c20c6dc3e398a9d89e5bdfd073
Delphi <292314+Delphi@users.noreply.github.com>
2019-01-07 22:27:52 +0000

    Update Uno_vu_line.ino

--- Uno_vu_line.ino
+++ Uno_vu_line.ino
@@ -17,6 +17,7 @@
 # define COLOR_ORDER GRB        // Try mixing up the letters (RGB, GBR, BRG, etc) for a whole new world of color combinations
 # define BRIGHTNESS 200         // 0-255, higher number is brighter.
 # define LED_TYPE WS2812B       // Probably WS2812B
+# define MAX_LED_POWER 2000     // Limit power consumption of the LED strip in milliAmps. 
 # define DC_OFFSET 0            // DC offset in aux signal - if unusure, leave 0
 # define NOISE 10               // Noise/hum/interference in aux signal
 # define SAMPLES 32             // Length of buffer for dynamic level adjustment
@@ -25,6 +26,9 @@
 # define N_PIXELS_HALF (N_PIXELS / 2)
 # define PATTERN_TIME 10         // Seconds to show eaach pattern on auto
 # define TWO_STRIPS             // Comment out this line if only using one strip attached to the left channel
+//# define TWO_STRIPS_REVERSE      // Comment out this line if 2 strips are attached but you wish to mirror the left channel on the right.
+
+bool LEDShowEnable = false;     //Use by LED pattens to enable/disable LED updates.
 
 uint8_t volCountLeft = 0;       // Frame counter for storing past volume data
 int volLeft[SAMPLES];           // Collection of prior volume samples
@@ -41,6 +45,8 @@ int maxLvlAvgRight = 512;
 CRGB ledsLeft[N_PIXELS];
 #ifdef TWO_STRIPS
   CRGB ledsRight[N_PIXELS];
+#elif TWO_STRIPS_REVERSE
+  CRGB ledsLeftReverse[N_PIXELS];
 #endif
 
 uint8_t myhue = 0;
@@ -80,6 +86,8 @@ void setup() {
   FastLED.addLeds < LED_TYPE, LEFT_OUT_PIN, COLOR_ORDER > (ledsLeft, N_PIXELS).setCorrection(TypicalLEDStrip);
   #ifdef TWO_STRIPS
     FastLED.addLeds < LED_TYPE, RIGHT_OUT_PIN, COLOR_ORDER > (ledsRight, N_PIXELS).setCorrection(TypicalLEDStrip);
+  #elif TWO_STRIPS_REVERSE
+    FastLED.addLeds < LED_TYPE, RIGHT_OUT_PIN, COLOR_ORDER > (ledsLeftReverse, N_PIXELS).setCorrection(TypicalLEDStrip);
   #endif
   FastLED.setBrightness(BRIGHTNESS);
 
@@ -91,7 +99,7 @@ void setup() {
 }
 
 void loop() {
-
+  
   // Read button
   modeBtn.read(); 
   switch (state) {
@@ -210,6 +218,14 @@ void loop() {
       rainbow(10);
       break;
   }
+  
+  #ifdef TWO_STRIPS_REVERSE
+    ReverseCopyLeftStripOnToRightStrip();
+  #endif
+  if(LEDShowEnable==true) {
+    FastLED.Show();
+    LEDShowEnable=false;
+  }
 }
 
 // ------------------
@@ -305,4 +321,13 @@ void averageReadings(uint8_t channel) {
     minLvlAvgRight = (minLvlAvgLeft * 63 + minLvl) >> 6; // Dampen min/max levels
     maxLvlAvgRight = (maxLvlAvgLeft * 63 + maxLvl) >> 6; // (fake rolling average)
   }
+}
+
+//function to copy the left strip onto the right strip in reverse order.
+void ReverseCopyLeftStripOnToRightStrip() {
+  #ifdef TWO_STRIPS_REVERSE
+    for(int i=0; i<N_PIXELS; i++) {
+      ledsLeftReverse[N_PIXELS-i] = ledsLeft[i];
+    }
+  #endif
 }
