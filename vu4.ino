a6bc15f3a7870c58d7f72ae8b7c2d9d506f7e7d7
Delphi <292314+Delphi@users.noreply.github.com>
2019-01-07 22:12:51 +0000

    Update vu4.ino

--- vu4.ino
+++ vu4.ino
@@ -65,7 +65,7 @@ void vu4(bool is_centered, uint8_t channel) {
 
   averageReadings(channel);
 
-  FastLED.show();
+  //FastLED.show();
 }
 
 uint8_t rainbowHue2(uint8_t pixel, uint8_t num_pixels) {
